fddc7c668529ec3e3fbe5b1c1dbf27a6997d42a0
Robert Schroll <rschroll@gmail.com>
2013-11-22 01:29:26 -0300

    Initial commit

---
+++ convert.js
@@ -0,0 +1,88 @@
+function convert() {
+    var input = document.getElementById("input").value;
+    var italic = document.getElementById("italic").checked;
+    var output = stringify(tokenize(input), italic);
+    document.getElementById("htmloutput").innerHTML = output;
+    document.getElementById("phpbboutput").innerHTML = output.replace(/</g, "[").replace(/>/g, "]");
+    return false;
+}
+
+function autoconvert() {
+    if (document.getElementById("auto").checked)
+        return convert();
+    return true;
+}
+
+function tokenize(string) {
+    var tokens = [];
+    for (var i=0; i<string.length; i++) {
+        var token = string[i];
+        if (token == "\\" && i+1 < string.length) {
+            while (i+1 < string.length && isletter(string[i+1]))
+                token += string[++i];
+            if (token == "\\" && i+1 < string.length) // Non-letter followed backslash
+                token += string[++i];
+            while (i+1 < string.length && string[i+1] == " ")
+                i += 1;
+        }
+        tokens.push(symbol(token));
+    }
+    return tokens;
+}
+
+function symbol(key) {
+    var s = SYMBOLS[key];
+    return (s === undefined) ? key : s;
+}
+
+function isletter(char) {
+    var code = char.charCodeAt(0);
+    return (64 < code && code < 91) || (96 < code && code < 123);
+}
+
+MARKUPS = {
+    "_": "sub",
+    "^": "sup",
+    "\\mathit": "i",
+    "\\mathbf": "b",
+}
+
+function stringify(tokens, italicize_latin) {
+    var string = "";
+    var pending = [];
+    var bracket_close = [];
+    for (var i=0; i<tokens.length; i++) {
+        var markup = MARKUPS[tokens[i]];
+        if (markup !== undefined) {
+            pending.push(markup);
+            continue;
+        }
+        
+        if (tokens[i] == "{") {
+            bracket_close.push(pending);
+            string += open_tags(pending);
+            pending = [];
+            continue;
+        }
+        
+        if (tokens[i] == "}") {
+            string += close_tags(bracket_close.pop());
+            pending = []; // Should already be empty
+            continue;
+        }
+        
+        if (isletter(tokens[i]) && italicize_latin)
+            pending.push("i");
+        string += open_tags(pending) + tokens[i] + close_tags(pending);
+        pending = [];
+    }
+    return string;
+}
+
+function open_tags(pending) {
+    return pending.length ? "<" + pending.join("><") + ">" : "";
+}
+
+function close_tags(pending) {
+    return pending.length ? "</" + pending.reverse().join("></") + ">" : "";
+}
